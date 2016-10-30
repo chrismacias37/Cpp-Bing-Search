7c1f5b76202f62255ffb5a04d12a2e8caac5b72d
Christian Macias <chrismacias37@gmail.com>
2016-10-30 17:16:15 -0600

    new file:   bing_search.cpp 	new file:   bing_search.hpp

---
+++ bing_search.hpp
@@ -0,0 +1,53 @@
+/*
+ * bing_search.hpp
+ *
+ *  Created on: Oct 26, 2016
+ *      Author: chris
+ */
+
+#ifndef BING_SEARCH_HPP_
+#define BING_SEARCH_HPP_
+
+#define USER_AGENT "User-Agent: Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 822)"
+
+extern "C"
+{
+#include<curl/curl.h>
+#include<csv.h>
+#include"cJSON.h"
+#include<stdlib.h>
+#include<stdio.h>
+}
+
+#include<string>
+#include<iostream>
+#include <cstdint>
+#include <cstring>
+
+class bing_search
+{
+public:
+	bing_search();
+	int set_key(std::string api_key);
+	int image_search(std::string keywords);
+	std::string result_format(int result_index_number);
+	std::string *result_url(int result_index_number);
+
+
+private:
+
+	std::string api_key;
+	curl_slist *headers;
+	CURL *bing_crl_hndl;
+	static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp);
+	struct MemoryStruct {
+	  char *memory;
+	  size_t size;
+	}json_txt;
+
+
+};
+
+
+
+#endif /* BING_SEARCH_HPP_ */
